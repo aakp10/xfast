263cf2969651e883355bd2158ff0fe8e12c1f069
Karuna Grewal <karunagrewal98@gmail.com>
2019-11-11 22:04:19 +0530

    Add insert node fn

--- src/trie.rs
+++ src/trie.rs
@@ -151,7 +151,7 @@ impl<T> Xfast<T> {
 
     fn find_successor(&self, key: usize) -> Option<&TrieNode<T>> {
         // find the lowest common ancestor- a node which shares maximum common prefix with the key
-        let mut successor_node: Option<*mut TrieNode<T>> = self.find_lowest_common_ancestor(key);
+        let successor_node: Option<*mut TrieNode<T>> = self.find_lowest_common_ancestor(key);
         match successor_node {
             Some(mut node) => unsafe {
                 // successor of a key already present is the key itself
@@ -191,7 +191,7 @@ impl<T> Xfast<T> {
 
     fn find_predecessor(&self, key: usize) -> Option<&TrieNode<T>> {
         // find the lowest common ancestor- a node which shares maximum common prefix with the key
-        let mut predecessor_node: Option<*mut TrieNode<T>> = self.find_lowest_common_ancestor(key);
+        let predecessor_node: Option<*mut TrieNode<T>> = self.find_lowest_common_ancestor(key);
         match predecessor_node {
             Some(mut node) => unsafe {
                 // predecessor of a key already present is the key itself
@@ -223,6 +223,149 @@ impl<T> Xfast<T> {
                 return None;
             }
         }
+    }
+
+    fn populate_internal_nodes(&mut self, key: usize) {
+        let mut level = 1;
+        let max_levels = self.nr_levels;
+        while level < max_levels {
+            let prefix = key >> (max_levels - level);
+            if let None = self.level_maps[level].get(&prefix) {
+                let temp_node = TrieNode::new_internal(level);
+                let temp_node = Box::into_raw_non_null(temp_node);
+                self.level_maps[level].insert(prefix, temp_node);
+                // add to the right child if the bit is 1 at that index else make it the left child
+                if (prefix & 1) != 0 {
+                    let temp_prefix = prefix >> 1;
+                    self.level_maps[level-1].get(&temp_prefix).map(|&value| unsafe{
+                        (*value.as_ptr()).right = Some(temp_node);
+                        (*value.as_ptr()).is_desc_right = false;
+                    });
+                }
+                else {
+                    let temp_prefix = prefix >> 1;
+                    self.level_maps[level-1].get(&temp_prefix).map(|&value| unsafe{
+                        (*value.as_ptr()).left = Some(temp_node);
+                        (*value.as_ptr()).is_desc_left = false;
+                    }); 
+                }
+            }
+            level += 1;
+        }
+    }
+
+    fn update_descendant_ptr(&mut self, key: usize) {
+        let mut prefix = key;
+        let mut level = self.nr_levels - 1;
+
+        while level > 0 {
+            prefix = prefix >> 1;
+            // find an internal node prefixed as `prefix` at `level` in the level_map
+            self.level_maps[level].get(&prefix).map(|&value| unsafe {
+                //check if this node has a left child
+                match (*value.as_ptr()).left {
+                    //the internal node doesn't have a left child
+                    None => {
+                        //An internal node is inserted in a trie only when it has one its children
+                        //Therefore, this node has a right child which is used to find its descendant ptr
+                        (*value.as_ptr()).right.map(|right_node| {
+                            (*value.as_ptr()).left = TrieNode::get_leftmost_node(self.nr_levels, right_node.as_ptr());
+                            (*value.as_ptr()).is_desc_left = true;
+                        });
+                    },
+                    // Left child is present
+                    Some(left_ptr) => {
+                        //this internal node can have a right child or not
+                        match (*value.as_ptr()).right {
+                            // the right child is not present
+                            None => {
+                                (*value.as_ptr()).right = TrieNode::get_rightmost_node(self.nr_levels, left_ptr.as_ptr());
+                                (*value.as_ptr()).is_desc_right = true;
+                            }
+                            // right child is also present
+                            Some(right_ptr)=> {
+                                // if any of the left or the right child is associated with a descendant pointer then update with the latest descendant pointer. 
+                                // At any given instance only one descendant ptr can be present
+                                if (*value.as_ptr()).is_desc_right {
+                                    (*value.as_ptr()).right = TrieNode::get_rightmost_node(self.nr_levels, left_ptr.as_ptr());
+                                }
+                                else if (*value.as_ptr()).is_desc_left {
+                                    (*value.as_ptr()).left = TrieNode::get_leftmost_node(self.nr_levels, right_ptr.as_ptr());
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+            level -= 1;
+        }
+
+        // update the descendant ptr for the root node
+        self.level_maps[0].get(&0).map(|&value| unsafe {
+            let is_left_descendant = (*value.as_ptr()).is_desc_left;
+            let is_right_descendant = (*value.as_ptr()).is_desc_right;
+            if is_left_descendant {
+                (*value.as_ptr()).right.map(|right_node| {
+                    (*value.as_ptr()).left = TrieNode::get_leftmost_node(self.nr_levels, right_node.as_ptr());
+                });
+            }
+            if is_right_descendant {
+                println!("editing root");
+                (*value.as_ptr()).left.map(|left_node| {
+                    (*value.as_ptr()).right = TrieNode::get_rightmost_node(self.nr_levels, left_node.as_ptr());
+                });
+            }
+        });
+    }
+
+    pub fn insert_key(&mut self, key: usize, value: T) {
+        //create a new node with key and val
+        let new_node = TrieNode::new(key, value, self.nr_levels);
+        let new_node = Some(Box::into_raw_non_null(new_node));
+        //find predecessor and successor for the new node
+        let predecessor = self.find_predecessor(key);
+        let successor = self.find_successor(key);
+        
+        //update the right and left pointers of the new node to refer to its successors and predecessors resp.
+        //update the right ptr in the predecessor ,and left ptr in the successor with the new_node.
+        predecessor.map(|pred_node| unsafe{
+            //FIXME
+            let pred_node = &(*pred_node) as *const TrieNode<T> as *mut TrieNode<T>;
+            new_node.map(|node| {
+                (*node.as_ptr()).right = (*pred_node).right;
+                (*node.as_ptr()).left = NonNull::new(pred_node);
+            });
+            (*pred_node).right = new_node;
+        });
+
+        successor.map(|suc_node| unsafe{
+            let suc_node = &(*suc_node) as *const TrieNode<T> as *mut TrieNode<T>;
+            new_node.map(|node| {
+                (*node.as_ptr()).left = (*suc_node).left;
+                (*node.as_ptr()).right = NonNull::new(suc_node);
+            });
+            (*suc_node).left = new_node;
+        });
+
+        //populate intermediate iternal nodes on the path down the new_node
+        self.populate_internal_nodes(key);
+        
+        //insert the new_node at the last level and update the ptr of its parent node using the prefix bit
+        self.level_maps[self.nr_levels].insert(key, new_node.unwrap());
+        let temp_key = key >> 1;
+        self.level_maps[self.nr_levels-1].get(&temp_key).map(|&value| unsafe {
+            if (key & 1) != 0 {
+                (*value.as_ptr()).right= new_node;
+                (*value.as_ptr()).is_desc_right = false;
+            }
+            else {
+                (*value.as_ptr()).left = new_node;
+                (*value.as_ptr()).is_desc_left = false;
+            }
+        });
+
+        // update descendant ptrs
+        self.update_descendant_ptr(key);
     }
 
 }
