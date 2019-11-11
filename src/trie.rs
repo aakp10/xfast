69c34e0bcca816bfbbe7377fbfb1f93c24902156
Karuna Grewal <karunagrewal98@gmail.com>
2019-11-11 19:22:07 +0530

    Implement constructor for Xfast

--- src/trie.rs
+++ src/trie.rs
@@ -3,12 +3,14 @@ use std::ptr::NonNull;
 
 type Node<T> = NonNull<TrieNode<T>>;
 #[derive(Debug)]
-struct TrieNode<T> {
+pub struct TrieNode<T> {
     key: usize,
     value: Option<T>,
     level: usize,
     right: Option<NonNull<TrieNode<T>>>,
     left: Option<NonNull<TrieNode<T>>>,
+    is_desc_left: bool,
+    is_desc_right: bool,
 }
 
 #[derive(Debug)]
@@ -25,7 +27,9 @@ impl<T> TrieNode<T> {
             value: Some(value),
             level: level,
             right: None,
-            left: None, 
+            left: None,
+            is_desc_right: true,
+            is_desc_left: true,
         })
     }
 
@@ -36,7 +40,9 @@ impl<T> TrieNode<T> {
             value: None,
             level,
             right: None,
-            left: None, 
+            left: None,
+            is_desc_left: true,
+            is_desc_right: true,
         })
     }
 
@@ -81,3 +87,40 @@ impl<T> TrieNode<T> {
     }
 }
 
+impl<T> Xfast<T> {
+    
+    pub fn new(range: usize) -> Self {
+        let nr_levels = Xfast::<T>::get_levels_count(range);
+        let level_maps = Xfast::create_map_list(nr_levels+1);
+        let mut new_trie = Xfast {
+            nr_levels,
+            level_maps,
+        };
+
+        // insert the root node in the trie at level 0
+        let root_node = TrieNode::new_internal(0);
+        let root_node = Box::into_raw_non_null(root_node);
+        new_trie.level_maps[0].insert(0, root_node);
+        new_trie
+    }
+
+    // levels => height of the trie
+    fn get_levels_count(mut range: usize) -> usize {
+        let mut levels = 0;
+        while range > 0 {
+            range >>= 1;
+            levels += 1;
+        }
+        levels
+    }
+
+    // helper fn for populating a vector list of hashmaps
+    fn create_map_list(nr_levels: usize) -> Vec<HashMap<usize, Node<T>>> {
+        let mut map_list: Vec<HashMap<usize, Node<T>>> = Vec::new();
+        for _level in 0..nr_levels {
+            let level_hash: HashMap<usize, Node<T>> = HashMap::new();
+            map_list.push(level_hash);
+        }
+        map_list
+    }
+}
